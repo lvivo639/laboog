80eb8cbf444d4a3becf87f0cb1320b65ec7c455e
volmk <lvi.work1@gmail.com>
2021-01-16 19:22:07 +0200

    register loading fix

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,47 +1,39 @@
-console.log("Loading Browser JS app.js");
+console.log("Loading Browser JS app.js")
 
-function register() {
-   const form = new FormData(document.getElementById('reg-form'));
-   Promise.all([
-
-      fetch("/templates/register.mst").then(x => x.text()),
-      fetch('/api/v1/users/create', {
-         method: 'POST',
-         body: form
-      }).then(x => x.json())
-   ])
-      .then(([templateStr, itemsData]) => {
-         if (!itemsData.login) return Mustache.render(templateStr, itemsData);
-
-         console.log(window.location.origin + "/auth/login?login=" + itemsData.login);
-         window.location.replace(window.location.origin + "/auth/login?login=" + itemsData.login);
-         Loading();
-
-      })
-      .then(htmlStr => {
-         const appEl = document.getElementById('app');
-         appEl.innerHTML = htmlStr;
-      })
-      .catch(err => console.log(err));
-};
+const appEl = document.getElementById('app')
 
 function Loading() {
-   fetch("/templates/loading.mst").then(x => x.text())
-      .then(htmlStr => {
-         const appEl = document.getElementById('app');
-         appEl.innerHTML = htmlStr;
-      })
-      .catch(err => console.error(err));
 }
 
-Loading();
-fetch("/templates/register.mst").then(x => x.text())
-    .then((templateStr) => {
-        return Mustache.render(templateStr, {});
+async function register() {
+    const form = new FormData(document.getElementById('reg-form'))
+    await Loading()
+    Promise.all([
+        fetch("/templates/register.mst")
+            .then(x => x.text()),
+        fetch('/api/v1/users/create', {
+            method: 'POST',
+            body: form
+        })
+            .then(x => x.json())
+    ])
+        .then(([templateStr, itemsData]) => {
+            if (!itemsData.login) return Mustache.render(templateStr, itemsData)
+            console.log(window.location.origin + "/auth/login?login=" + itemsData.login)
+            window.location.replace(window.location.origin + "/auth/login?login=" + itemsData.login)
+        })
+        .then(htmlStr => {
+            appEl.innerHTML = htmlStr
+        })
+        .catch(err => console.log(err))
+}
 
+fetch("/templates/register.mst")
+    .then(x => x.text())
+    .then((templateStr) => {
+        return Mustache.render(templateStr, {})
     })
     .then(htmlStr => {
-        const appEl = document.getElementById('app');
-        appEl.innerHTML = htmlStr;
+        appEl.innerHTML = htmlStr
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
